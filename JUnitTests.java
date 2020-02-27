c995d87b9afe3d742e925f8910d59b10e4e20df4
Katherine <Katherine@DESKTOP-6DU8QAT>
2020-02-27 00:30:35 +0000

    finished basic tests.

--- JUnitTests.java
+++ JUnitTests.java
@@ -1,19 +1,16 @@
 import static org.junit.Assert.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import java.util.TreeSet;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import org.junit.*;
-
 public class JUnitTests {
 	PassengerList pList = new PassengerList();
-	
+	FlightList fList = new FlightList(pList);
+
 	@Before
 	public void setUp() {
-		
+
 		Boolean ci1 = Boolean.parseBoolean("TRUE");
 		int bref1 = 44850;
 		Name name1 = new Name("Millie Villages");
@@ -27,13 +24,35 @@ public class JUnitTests {
 		String fc2 = "GH6494";
 		Passenger p2 = new Passenger(bref2, name2, fc2, ci2, 0,0,0);
 		pList.add(p2);
+		
+		int bref3 = 48634;
+		Name name3 = new Name("Mark McDonald");
+		Passenger p3 = new Passenger(bref3, name3, fc1, ci1, 0, 0.50, 3);
+		pList.add(p3);
+
+		String d1 = "Dublin";
+		String c1 = "British Airways";
+		int pCap1 = 70;
+		double bwCap1 = 300;
+		double bvCap1 = 30;
+		Flight f1 = new Flight(fc1, d1, c1, pCap1, bwCap1, bvCap1);
+		fList.add(f1);
+
+		String d2 = "Belfast";
+		String c2 = "Rayenair";
+		int pCap2 = 50;
+		double bwCap2 = 200;
+		double bvCap2 = 25;
+		Flight f2 = new Flight(fc2, d2, c2, pCap2, bwCap2, bvCap2);
+		fList.add(f2);
 	}
 
-//	@After
-//	public void tearDown() {
-//		PassengerList passengerList = new PassengerList();
-//	}
-	
+		@After
+		public void tearDown() {
+			PassengerList pList = new PassengerList();
+			FlightList fList = new FlightList(pList);
+		}
+
 	@Test		//Test Passenger name can be found and converted.
 	public void testGetPassengerName() {
 		Boolean tci = Boolean.parseBoolean("FALSE");
@@ -47,25 +66,48 @@ public class JUnitTests {
 		String actual1 = name1.getFirstName();		
 		assertEquals(message1, expected1, actual1);
 	}
-	
+
 	@Test		//Test if Passenger search works. (name method already tested)
 	public void testFindPassenger() {
-		 String tbref1 = "84629";
-		 String tname1 = "Brown";
-		// Passenger expected1 = new Passenger(84629, new Name("Alison Brown"),
-		//		 "GH6494", Boolean.parseBoolean("FALSE"), 0,0,0);
-		 String expected1 = "Alison";
-		 String message1 = "Find existing Passenger method failed";		 
-		 Passenger tp1 = pList.findPassenger(tbref1, tname1);		 
-		 String actual1 = tp1.getPassengerName().getFirstName();
-		 assertEquals(message1, expected1, actual1);
-		 
-		 String tbref2 = "00000";
-		 String tname2 = "IncorrectName";
-		 String message2 = "Finding fake Passenger method failed";		 
-		 Passenger actual2 =  pList.findPassenger(tbref2, tname2);	
-		 assertEquals(message2, null, actual2);
+		String tbref1 = "84629";
+		String tname1 = "Brown";
+		String expected1 = "Alison";
+		String message1 = "Find existing Passenger method failed";		 
+		Passenger tp1 = pList.findPassenger(tbref1, tname1);		 
+		String actual1 = tp1.getPassengerName().getFirstName();
+		assertEquals(message1, expected1, actual1);
+
+		String tbref2 = "00000";
+		String tname2 = "IncorrectName";
+		String message2 = "Finding fake Passenger method failed";		 
+		Passenger actual2 =  pList.findPassenger(tbref2, tname2);	
+		assertEquals(message2, null, actual2);
 	}	
-	
+
+	@Test		//Test if Flight search works. (getFlightCode only simply return method)
+	public void testFindByFlightCode() {
+		String tfc1 = "GH6494";
+		String expected1 = "GH6494";
+		String message1 = "Find existing Flight method failed";		 
+		Flight tf1 = fList.findByFlightCode(tfc1);		 
+		String actual1 = tf1.getFlightCode();
+		assertEquals(message1, expected1, actual1);
+
+		String tfc2 = "00000";
+		String message2 = "Finding fake Flight code method failed";
+		Flight actual2 = fList.findByFlightCode(tfc2);
+		assertEquals(message2, null, actual2);
+	}	
+
+	@Test
+	public void testGetWeightByFlight() {
+		String message1 = "Finding the passenger list of flight method failed";		 
+		String tfc1 = "EI3259";
+		double actual1 = pList.getWeightByFlight(tfc1);		
+		double expected1 = 0.6;
+		boolean test = expected1 == actual1 ;
+		assertEquals(message1,  Boolean.parseBoolean("TRUE"),  test);
+	}
+
 
 } //System.out.println(pList);
