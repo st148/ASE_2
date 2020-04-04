a55e1b470857a818a60b3f374a28d456562ab05a
Katherine <Katherine@192.168.0.83>
2020-04-04 13:48:11 +0100

    added airport class.

--- Manager.java
+++ Manager.java
@@ -2,20 +2,17 @@ public class Manager {
 	
 	private PassengerList allPassengers;
 	private FlightList allFlights;
+	private Airport airportQueue;
+	
 	public Manager() {
 		allPassengers = new PassengerList();
 		allFlights = new FlightList(allPassengers);
+		airportQueue = new Airport();
 	}
     
 	public void run() {
-		
 		allFlights.readFile("Flight_mock.csv");
 		allPassengers.readFile("Passenger_mock.csv");
-		
-
-		CheckInGUI gui = new CheckInGUI(allFlights, allPassengers);
-        	gui.setVisible(true);
-		
-
+		airportQueue.PassengerListToQueue();	
 	}
 }
