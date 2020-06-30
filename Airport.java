a9fb23ae4bac6c5717522e44a2ffb5b5cd7b0141
SydColvin <57715986+SydColvin@users.noreply.github.com>
2020-06-30 19:51:47 +0100

    Update Airport.java
    Added desk numbers to each check-in process.

--- Airport.java
+++ Airport.java
@@ -1,23 +1,51 @@
 import java.util.TreeSet;
 import java.util.concurrent.Semaphore;
+import java.util.Arrays;
 
 public class Airport {
 	int numberOfDesks = 2;    
-	private Semaphore Desk = new Semaphore(numberOfDesks);
+	private Semaphore freeDesk = new Semaphore(numberOfDesks);
+	//creates array no. of desks long auto set to 0 (waiting)
+	//we will change so 1 is serving and 2 is closed
+	public int[] deskStatus = new int [numberOfDesks];
 
 	class CheckIn{
+		// defining which desk is in use
+		public int currantDesk = 999; //error number
 		//count the total number of check ins
 		int noCheckIns = 0;
 		public int getC () {return this.noCheckIns;};
-		public void useDesk() {		
+		public void useDesk() {	
 			// Include any check in code required for GUI.
 
-			
+
+
+			// add returnable desk status 
+			System.out.println("using desk statuses "+Arrays.toString(deskStatus));
+
 			//increase the count of checkins
 			int n = this.noCheckIns; n++;
 			this.noCheckIns = n;		// the counting is mostly for testing purposes rn
 			String threadName = Thread.currentThread().getName();
-			System.out.println("Check-In " +noCheckIns + " (" + threadName + ") in progress.");
+			System.out.println("Check-In " + noCheckIns + " (" + threadName + ") at desk " + currantDesk);
+		}  
+		
+		
+		public int identifyFreeDeskNo() {
+			int n = 999; //error number 
+			for (int i = 0; i < deskStatus.length; i++) {
+				if (deskStatus[i] == 0) {
+					deskStatus[i] = 1;
+					n = i+1; 
+					this.currantDesk = n; //System.out.println("useing desk no.:-" + n + " [" + currantDesk + "]");
+					break;
+				}
+			}
+			return n;
+		}		
+		public void emptyIdentifiedDesknoNo(int n) {
+			deskStatus[n-1] = 0; 
+			this.currantDesk = 999; //System.out.println("emtied desk no.:-" + n + " [" + currantDesk + "]");
 		}
 	}
 
@@ -27,18 +55,26 @@ public class Airport {
 	class PassengerInQueue  implements Runnable {
 		public void run(){
 			String NameBRef = Thread.currentThread().getName();
-			System.out.println(NameBRef + " joined queue.");	
+			System.out.println(NameBRef + " joined queue.");
+
+			// add passenger to returnable queue list 
+
 			try { 
-				Desk.acquire();
+				freeDesk.acquire();
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+			// add removal from the queue list 
+			
+			int deskno = checkin.identifyFreeDeskNo();
 			checkin.useDesk();
-			Desk.release();
+			checkin.emptyIdentifiedDesknoNo(deskno);
+			freeDesk.release();
 			System.out.println(NameBRef + " completed check in");
 		}	
 	}
+
 
 	//creates running thread for every passenger in list
 	public void PassengerListToQueue() {
@@ -55,75 +91,4 @@ public class Airport {
 
 }
 
-//	public void PassengerCheckIn(Passenger p, int Desk) {
-//		// Do the checkin Things.
-//		String name = p.getPassengerName().getFirstAndLastName();
-//		//System.out.println(name + "Check in has happened");
-//	}	
-//	String name;
-//	Thread t;
-//	
-//	PassengerThread (String thread){
-//		name = "PassengerToQueue";  
-//		t = new Thread(this, name);
-//		System.out.println(" Passenger active. New thread: " + t);
-//		t.start();
-//	}		 
-//
-//	public void run() {
-////		try {	
-////			System.out.println(" PassengerThread running.");
-////			// add code to include passenger here
-////		}catch (InterruptedException e) {
-////			System.out.println(name + "Interrupted");
-////		}
-//		System.out.println(name + " exiting.");
-//	}
-
-//
-//public class DeskThread extends Thread{
-//	String name;
-//	Thread t;
-//	DeskThread (String thread){
-//		name = "CheckInDeskAdded";  
-//		t = new Thread(this, name);
-//		System.out.println(" Desk active. New thread: " + t);
-//		t.start();
-//	}	
-//
-//	public void run() {
-//		try {
-//			for(int i = 2; i > 0; i--) {
-//				// add code so passenger "uses" desk
-//				System.out.println(name + ": " + i);
-//				Thread.sleep(1000);
-//			}
-//		}catch (InterruptedException e) {
-//			System.out.println(name + "Interrupted");
-//		}
-//		System.out.println(name + " exiting.");
-//	}
-//}
-//
-
-
-//		    int availableSlots() {
-//		        return Desk.availablePermits();
-//		    }
-
-
-//		class MultiThread {
-//		public static void main(String args[]) {
-//		     new MyThread("One");
-//		     new MyThread("Two");
-//		     new NewThread("Three");
-//		try {
-//		     Thread.sleep(10000);
-//		} catch (InterruptedException e) {
-//		      System.out.println("Main thread Interrupted");
-//		}
-//		      System.out.println("Main thread exiting.");
-//		      }
-//		}
-//	
 
